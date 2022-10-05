08088c65bc6fe97035d87787535f9ecfa20ac63a
haider <haider.n@somaiya.edu>
2022-10-05 12:02:11 +0530

    helmet added

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const swagger_1 = require("@nestjs/swagger");
+const helmet_1 = require("helmet");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('docs', app, document);
+    app.use((0, helmet_1.default)());
     await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+  app.use(helmet());
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
